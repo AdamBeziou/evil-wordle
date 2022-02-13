eef7c6c254bb80cf684aacf1396b27ce9824b2d1
AdamBeziou <adam.beziou@gmail.com>
2022-02-12 22:53:31 -0700

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import words from "./words.js";
+import Guess from "./Guess";
+import { getWord } from './Util'
+
+const MAX_GUESSES = 5;
+const WORD_LENGTH = 5;
+let currentGuessIndex = 0;
+
+const word = getWord(WORD_LENGTH, words);
+const game = new Game(MAX_GUESSES, word);
+
+document.addEventListener("keydown", game.keyPressed);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>WORDLE CLONE</p>
+        {game.guesses.map((guess) => (
+          <Guess
+            guess={guess}
+            correctWord={game.correctWord}
+            showComparison={false}
+          />
+        ))}
       </header>
     </div>
   );
