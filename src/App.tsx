d957d1583136f6331c0914cc4e640d57f568fdf6
AdamBeziou <adam.beziou@gmail.com>
2022-02-20 14:56:38 -0700

    Add game view and get rid of app router

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,26 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import "./App.css";
-import words from "./Words/WordList.js";
-import Guess from "./Components/Guess";
-import AppRouter from "./AppRouter";
-
-const MAX_GUESSES = 5;
-const WORD_LENGTH = 5;
-
-/*
-What does the game need to have?
-- A correct word
-- A current guess
-- A record of past guesses
-
-What operations are there in the game?
-- Making a guess
-  - Need to figure out what letters are in place/present, but out of place
-  - Need to record the guess in a record of past guesses
-
-What is configurable?
-- Length of the correct word
-- Number of possible guesses
-- Size/words in a word pool
-*/
+import GameView from "./Components/GameView";
+import Home from "./Components/Home";
+import { GameType } from "./Games/GameFactory";
+import { BASE_URL, BASIC_GAME, EVIL_GAME } from "./Routes";
 
 function App() {
   return (
     <div className="App">
-      <nav className="app-header">
-        <a>Wordle</a>
-        <a>Evil Wordle</a>
-      </nav>
-      <AppRouter />
+      <BrowserRouter>
+        <header className="app-header">
+          Just Another Wordle Clone
+          <nav className="app-navigation">
+            <Link to={BASIC_GAME}>WORDLE</Link>
+            <Link to={EVIL_GAME}>EVIL WORDLE</Link>
+          </nav>
+        </header>
+          <Routes>
+                <Route path={BASE_URL} element={<Home />} />
+                <Route path={BASIC_GAME} element={<GameView gameType={GameType.VANILLA}/>} />
+            </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/GameView.tsx
+++ src/Components/GameView.tsx
@@ -1,6 +1,24 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Game from "../Games/Game"
+import createGame, { GameConfig, GameType } from "../Games/GameFactory"
 
-const GameView = ({ game }: { game: Game }) => {
+const DEFAULT_WORD_LENGTH = 5
 
+const GameView = ({ gameType }: { gameType: GameType }) => {
+    const [game, setGame] = useState<Game | undefined>()
+    useEffect(() => {
+        const config: GameConfig = {
+            maximumWordLength: DEFAULT_WORD_LENGTH,
+            minimumWordLength: DEFAULT_WORD_LENGTH,
+            gameType
+        }
+        const game = createGame(config)
+        setGame(game)
+    }, [gameType])
+
+    return (
+        <p>Test</p>
+    )
 }
+
+export default GameView
